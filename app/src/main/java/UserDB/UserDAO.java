f32c5a257536637bbd6ed0ad48b10c6d7eb89c41
Alexii78500 <apetignat@csumb.edu>
2023-04-22 17:35:46 -0700

    Create account working

--- app/src/main/java/UserDB/UserDAO.java
+++ app/src/main/java/UserDB/UserDAO.java
@@ -3,7 +3,10 @@ package UserDB;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface UserDAO {
@@ -16,4 +19,13 @@ public interface UserDAO {
 
     @Delete
     void Delete(User...users);
+
+    @Query("SELECT * FROM " + AppDataBaseUser.DATABASE_TABLE)
+    List<User> getUsers();
+
+    @Query("SELECT * FROM " + AppDataBaseUser.DATABASE_TABLE + " WHERE UserId = :userId")
+    List<User> getUserById(int userId);
+
+    @Query("SELECT * FROM " + AppDataBaseUser.DATABASE_TABLE + " WHERE name = :username")
+    List<User> getUserByName(String username);
 }
--- app/src/main/java/com/example/project_2_space_recorder/CreateAccountActivity.java
+++ app/src/main/java/com/example/project_2_space_recorder/CreateAccountActivity.java
@@ -8,8 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import com.example.project_2_space_recorder.databinding.CreateAccountBinding;
+
+import UserDB.AppDataBaseUser;
+import UserDB.User;
+import UserDB.UserDAO;
 
 public class CreateAccountActivity extends AppCompatActivity {
     
@@ -18,6 +23,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     EditText password;
     EditText password2;
     Button createButton;
+    UserDAO userDAO;
 
 
 
@@ -34,13 +40,37 @@ public class CreateAccountActivity extends AppCompatActivity {
         password2 = mainBinding.editTextReenterPassword;
         createButton = mainBinding.buttonCreateAccount;
 
+        userDAO = Room.databaseBuilder(this, AppDataBaseUser.class, AppDataBaseUser.DATABASE_NAME)
+                .allowMainThreadQueries().build().UserDAO();
+
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = MainActivity.getIntent(getApplicationContext());
-                startActivity(intent);
+                if (createUser()) {
+                    Intent intent = MainActivity.getIntent(getApplicationContext());
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    boolean createUser(){
+        String un = username.getText().toString();
+        System.out.println(un);
+        if (userDAO.getUserByName(un).isEmpty()){
+            String pw = password.getText().toString();
+            String pw2 = password2.getText().toString();
+            if (!pw.equals("") && pw.equals(pw2)){
+                userDAO.Insert(new User(un, pw, 0));
+                System.out.println("User Created");
+                return true;
+            }
+            //Error: not same password
+            System.out.println("Both passwords are different");
+        }
+        //Error: username taken
+        System.out.println("Username already taken");
+        return false;
     }
 
     public static Intent getIntent(Context context){
